45e47bdf9462ed6e674e8c8852b9d47017739f84
googoocone <googoocone1@naver.com>
2025-02-23 22:15:00 +0900

    택배상자꺼내기 코드 간결화

--- programmers_level_1/택배상자꺼내기/택배상자꺼내기.js
+++ programmers_level_1/택배상자꺼내기/택배상자꺼내기.js
@@ -6,19 +6,25 @@ function solution(n, w, num) {
   var location_x = 0;
   var location_y = 0;
 
+  //w와 n값의 범위가 정해졌기 때문에 일단 그에 맞춰서 2차원 행렬을 만든다.
+  //상자가 들어가면 1 아니면 0 그리고 num의 위치는 숫자 3으로 표시할 것이다.
   let Matrix = Array.from({ length: max_x }, () => Array(w).fill(0));
 
   for (let x = 0; x < max_x; x++) {
+    // 홀수면 왼 -> 오로 상자를 놓아야 하기 때문에
     if (x % 2 == 0) {
       for (let y = 0; y < w; y++) {
+        //주어진 상자의 갯수를 모두 다 놓으면 for문을 끝내야 하기 때문
         if (number == n) {
           break;
         }
-
         number++;
+
+        //상자를 놓으면서 행렬의 값을 1로 바꾼다.
         Matrix[x][y] = 1;
+
+        //상자를 놓으면서 꺼내려는 상자의 번호와 일치하게 되면, 행과 열의 정보를 담느다.
         if (number == num) {
-          Matrix[x][y] = 3;
           location_x = x;
           location_y = y;
         }
@@ -33,7 +39,6 @@ function solution(n, w, num) {
 
         Matrix[x][y] = 1;
         if (number == num) {
-          Matrix[x][y] = 3;
           location_x = x;
           location_y = y;
         }
@@ -45,7 +50,9 @@ function solution(n, w, num) {
     }
   }
 
-  for (let a = location_x + 1; a < max_x; a++) {
+  //상자의 위치에서 한칸씩 올라가면서 꺼내야하는 상자의 갯수를 카운트한다.
+  console.log(location_x, location_y);
+  for (let a = location_x; a < max_x; a++) {
     if (Matrix[a][location_y] == 1) {
       answer++;
     } else {
@@ -53,7 +60,7 @@ function solution(n, w, num) {
     }
   }
 
-  return answer + 1;
+  return answer;
 }
 
 let a = solution(22, 6, 8);
